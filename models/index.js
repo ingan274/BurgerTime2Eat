d75fa57ad2c399684efa5bd2765d856e2b36691f
ingan <isabelngan27@gmail.com>
2019-09-24 08:43:36 -0700

    updating index

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@
 // var path = require("path");
 // var Sequelize = require("sequelize");
 // var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
+// var env = process.env.NODE_ENV || "development";
 // var config = require(__dirname + "/../config/config.json")[env];
 // var db = {};
 
@@ -41,35 +41,28 @@ var env = process.env.NODE_ENV || "development";
 
 // module.exports = db;
 
-if (!global.hasOwnProperty('db')) {
-  var Sequelize = require('sequelize')
-    , sequelize = null
+var Sequelize = require('sequelize'), sequelize = null
+const http = require('http')
 
-  if (process.env.HEROKU_POSTGRESQL_AMBER_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_AMBER_URL, {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
-    })
-  } else {
-    // the application is executed on the local machine ... use mysql
-    sequelize = new Sequelize('example-app-db', 'root', null)
-  }
-
-  global.db = {
-    Sequelize: Sequelize,
-    sequelize: sequelize,
-    User:      sequelize.import(__dirname + '/user') 
-    // add your other models here
-  }
-
-  /*
-    Associations can be defined here. E.g. like this:
-    global.db.User.hasMany(global.db.SomethingElse)
-  */
+// checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+  })
 }
+
+
+// at the bottom of your script, this sets your server to listen for requests, after sequelize has been synced.
+// so if you already have your server listening for requests, maybe delete that code. I think, IDK i just copied this
+// off of documentation
+// db.  is assuming you already set sequelize on db
+db.sequelize.sync().then(function() {
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+});
+
 
 module.exports = global.db
